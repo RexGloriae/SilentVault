96a2ade560262adf0de0184f802dae8b1b11a2d9
Kessel <andreidanielcodreanu@gmail.com>
2025-10-29 12:36:38 +0200

    add pywrapper

---
+++ src/PythonWrapper/wrap.hxx
@@ -0,0 +1,12 @@
+/*
+Wrapper for calling functions from the Python module.
+Part of the Services category.
+*/
+#ifndef PY_WRAP_H
+#define PY_WRAP_H
+
+extern "C" {
+#include "python3/Python.h"
+}
+
+#endif  // PY_WRAP_H
