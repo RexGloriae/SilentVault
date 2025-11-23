2462f288984e70e7c202ed2539c1f2d443c467e9
Kessel <andreidanielcodreanu@gmail.com>
2025-11-23 17:16:34 +0200

    add server class

--- src/Server/include/server.hxx
+++ src/Server/include/server.hxx
@@ -1,4 +1,32 @@
 #ifndef SERVER
 #define SERVER
 
+#include <atomic>
+
+#include "comms.hxx"
+
+class Server {
+   private:
+    static int m_running;
+
+    Comms& m_conn;
+
+    Server(const Server& O) = delete;
+    Server(Server&& O) = delete;
+    Server operator=(const Server& O) = delete;
+    Server operator=(Server&& O) = delete;
+
+    void run_cli();
+
+    static void print(const std::string& text, bool new_line);
+
+   public:
+    Server(Comms& conn);
+    ~Server();
+
+    static std::atomic<bool> m_should_stop;
+
+    void start();
+};
+
 #endif  // SERVER
--- src/Server/src/server.cpp
+++ src/Server/src/server.cpp
@@ -1,1 +1,44 @@
 #include "../include/server.hxx"
+
+#include <cstring>
+#include <stdexcept>
+#include <thread>
+
+int               Server::m_running = 0;
+std::atomic<bool> Server::m_should_stop = false;
+
+Server::Server(Comms& conn) : m_conn(conn) {
+    m_running++;
+    if (m_running > 1) {
+        throw std::runtime_error("You can't declare more than one server");
+    }
+}
+
+Server::~Server() {};
+
+void Server::run_cli() {
+    Server::print("Enter \"help\" to see available commands.", true);
+    std::string cmd;
+    bool        run = true;
+    while (run) {
+        std::cin >> cmd;
+        if (strcasecmp("stop", cmd.c_str()) == 0) {
+            run = false;
+            m_should_stop = true;
+            m_conn.close_sock();
+        } else if (strcasecmp("stats", cmd.c_str()) == 0) {
+        }
+    }
+}
+
+void Server::print(const std::string& text, bool new_line) {
+    if (new_line == true) std::cout << "\n";
+    std::cout << "Server >$ " << text << std::endl;
+    std::cout << "\nServer >$ ";
+}
+
+void Server::start() {
+    std::thread cli_thread(&Server::run_cli, this);
+    m_conn.start_and_listen();
+    cli_thread.join();
+}
