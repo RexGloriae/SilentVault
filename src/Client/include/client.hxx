e34974c5c915ffdca95b75e7d31749b70487f04b
Kessel <andreidanielcodreanu@gmail.com>
2025-11-23 16:51:00 +0200

    add client class
    add init method

--- src/Client/include/client.hxx
+++ src/Client/include/client.hxx
@@ -1,4 +1,23 @@
 #ifndef CLIENT
 #define CLIENT
 
+#include "comms.hxx"
+
+class Client {
+   private:
+    Client(const Client& O) = delete;
+    Client(Client&& O) = delete;
+    Client& operator=(const Client& O) = delete;
+    Client& operator=(Client&& O) = delete;
+
+    static int m_running;
+    Comms&     m_conn;
+
+   public:
+    Client(Comms& conn);
+    ~Client();
+
+    void start(const char* host, int port);
+};
+
 #endif  // CLIENT
--- src/Client/src/client.cpp
+++ src/Client/src/client.cpp
@@ -1,1 +1,14 @@
 #include "../include/client.hxx"
+
+#include <stdexcept>
+
+int Client::m_running = 0;
+
+Client::Client(Comms& conn) : m_conn(conn) {
+    m_running++;
+    if (m_running > 1) {
+        throw std::runtime_error("You can't declare more than one client");
+    }
+}
+
+void Client::start(const char* host, int port) { m_conn.init(host, port); }
