2517363ab4449b790dafaa866f50b28bc96b2313
Kessel <andreidanielcodreanu@gmail.com>
2026-01-11 17:58:21 +0200

    add classes for different type of payloads

---
+++ src/Client/include/payload.hxx
@@ -0,0 +1,113 @@
+#ifndef PAYLOAD
+#define PAYLOAD
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+enum OPCODE : uint8_t {
+    POST_PUB_AND_SALT = 0x01,
+    GET_PUB_AND_SALT = 0x02,
+    GET_CHALLENGE = 0x03,
+    POST_RESPONSE = 0x04,
+    GET_AUTH_RESPONSE = 0x05,
+    POST_PUB_AND_SALT_REQUEST = 0x06,
+    POST_CHALLENGE_REQUEST = 0x07
+};
+
+void add_int(std::vector<char>& vec, int value);
+
+class Payload {
+   protected:
+    OPCODE            _opcode;
+    int               _user_len;
+    std::vector<char> _user;
+
+   public:
+    Payload(OPCODE opcode, std::string user);
+};
+
+class PostChallengeRequestPayload : Payload {
+   public:
+    PostChallengeRequestPayload(std::string user);
+    std::vector<char> serialize();
+};
+
+class PostPublicAndSaltPayload : protected Payload {
+   protected:
+    int               _pub_len;
+    std::vector<char> _pub;
+    int               _salt_len;
+    std::vector<char> _salt;
+
+   public:
+    PostPublicAndSaltPayload(std::string       user,
+                             std::vector<char> pub,
+                             std::vector<char> salt);
+    std::vector<char> serialize();
+
+    std::vector<char> pub() { return _pub; }
+    std::vector<char> salt() { return _salt; }
+};
+
+class PostResponsePayload : Payload {
+   private:
+    int               _pub_len;
+    std::vector<char> _pub;
+    int               _response_len;
+    std::vector<char> _response;
+    int               _r_bytes_len;
+    std::vector<char> _r_bytes;
+    int               _challenge_len;
+    std::vector<char> _challenge;
+
+   public:
+    PostResponsePayload(std::vector<char> pub,
+                        std::vector<char> response,
+                        std::vector<char> r_bytes,
+                        std::vector<char> challenge,
+                        std::string       user);
+
+    std::vector<char> serialize();
+};
+
+class GetPubAndSaltPayload : public PostPublicAndSaltPayload {
+   private:
+    std::vector<char> _payload;
+
+   public:
+    GetPubAndSaltPayload(std::vector<char> payload);
+    bool deserialize();
+};
+
+class GetChallengePayload : Payload {
+   private:
+    std::vector<char> _payload;
+    std::vector<char> _challenge;
+    int               _challenge_len;
+
+   public:
+    GetChallengePayload(std::vector<char> payload);
+    bool deserialize();
+
+    std::vector<char> challenge() { return _challenge; }
+};
+
+class GetAuthResponsePayload : Payload {
+   private:
+    std::vector<char> _payload;
+    bool              _response;
+
+   public:
+    GetAuthResponsePayload(std::vector<char> payload);
+    bool deserialize();
+    bool success() { return _response; }
+};
+
+class PostPublicAndSaltRequestPayload : Payload {
+   public:
+    PostPublicAndSaltRequestPayload(std::string user);
+    std::vector<char> serialize();
+};
+
+#endif  // PAYLOAD
---
+++ src/Client/src/payload.cpp
@@ -0,0 +1,224 @@
+#include "../include/payload.hxx"
+
+#include <cstring>
+
+void add_int(std::vector<char>& vec, int value) {
+    const char* bytes = reinterpret_cast<const char*>(&value);
+    vec.insert(vec.end(), bytes, bytes + sizeof(int));
+}
+
+Payload::Payload(OPCODE opcode, std::string user) {
+    _opcode = opcode;
+    for (char c : user) {
+        _user.push_back(c);
+    }
+    _user_len = user.size();
+}
+
+PostChallengeRequestPayload::PostChallengeRequestPayload(std::string user)
+    : Payload(POST_CHALLENGE_REQUEST, user) {}
+
+std::vector<char> PostChallengeRequestPayload::serialize() {
+    std::vector<char> buff;
+    buff.push_back(_opcode);
+    add_int(buff, _user_len);
+    for (char c : _user) {
+        buff.push_back(c);
+    }
+    return buff;
+}
+
+PostPublicAndSaltPayload::PostPublicAndSaltPayload(std::string       user,
+                                                   std::vector<char> pub,
+                                                   std::vector<char> salt)
+    : Payload(POST_PUB_AND_SALT, user) {
+    _pub = pub;
+    _salt = salt;
+    _pub_len = pub.size();
+    _salt_len = salt.size();
+}
+
+std::vector<char> PostPublicAndSaltPayload::serialize() {
+    std::vector<char> buff;
+    buff.push_back(_opcode);
+    add_int(buff, _user_len);
+    for (char c : _user) {
+        buff.push_back(c);
+    }
+    add_int(buff, _pub_len);
+    for (char c : _pub) {
+        buff.push_back(c);
+    }
+    add_int(buff, _salt_len);
+    for (char c : _salt) {
+        buff.push_back(c);
+    }
+    return buff;
+}
+
+PostResponsePayload::PostResponsePayload(std::vector<char> pub,
+                                         std::vector<char> response,
+                                         std::vector<char> r_bytes,
+                                         std::vector<char> challenge,
+                                         std::string       user)
+    : Payload(POST_RESPONSE, user) {
+    _pub = pub;
+    _response = response;
+    _r_bytes = r_bytes;
+    _response_len = response.size();
+    _pub_len = pub.size();
+    _r_bytes_len = r_bytes.size();
+    _challenge_len = challenge.size();
+    _challenge = challenge;
+}
+
+std::vector<char> PostResponsePayload::serialize() {
+    std::vector<char> buff;
+    buff.push_back(_opcode);
+    add_int(buff, _user_len);
+    for (char c : _user) {
+        buff.push_back(c);
+    }
+    add_int(buff, _pub_len);
+    for (char c : _pub) {
+        buff.push_back(c);
+    }
+    add_int(buff, _response_len);
+    for (char c : _response) {
+        buff.push_back(c);
+    }
+    add_int(buff, _r_bytes_len);
+    for (char c : _r_bytes) {
+        buff.push_back(c);
+    }
+    add_int(buff, _challenge_len);
+    for (char c : _challenge) {
+        buff.push_back(c);
+    }
+    return buff;
+}
+
+GetPubAndSaltPayload::GetPubAndSaltPayload(std::vector<char> payload)
+    : PostPublicAndSaltPayload({}, {}, {}) {
+    _payload = payload;
+}
+
+bool GetPubAndSaltPayload::deserialize() {
+    size_t index = 0;
+
+    if (_payload.empty()) return false;
+    _opcode = static_cast<OPCODE>(_payload[index++]);
+    if (_opcode != GET_PUB_AND_SALT) {
+        return false;
+    }
+
+    if (index + sizeof(int) > _payload.size()) return false;
+    memcpy(&_user_len, &_payload[index], sizeof(int));
+    index += sizeof(int);
+    if (_user_len < 0) return false;
+    if (index + _user_len > _payload.size()) return false;
+    _user.resize(_user_len);
+    for (int i = 0; i < _user_len; i++) {
+        _user[i] = _payload[index++];
+    }
+
+    if (index + sizeof(int) > _payload.size()) return false;
+    memcpy(&_pub_len, &_payload[index], sizeof(int));
+    index += sizeof(int);
+    if (_pub_len < 0) return false;
+    if (index + _pub_len > _payload.size()) return false;
+    _pub.resize(_pub_len);
+    for (int i = 0; i < _pub_len; i++) {
+        _pub[i] = _payload[index++];
+    }
+
+    if (index + sizeof(int) > _payload.size()) return false;
+    memcpy(&_salt_len, &_payload[index], sizeof(int));
+    index += sizeof(int);
+    if (_salt_len < 0) return false;
+    if (index + _salt_len > _payload.size()) return false;
+    _salt.resize(_salt_len);
+    for (int i = 0; i < _salt_len; i++) {
+        _salt[i] = _payload[index++];
+    }
+
+    return true;
+}
+
+GetChallengePayload::GetChallengePayload(std::vector<char> payload)
+    : Payload({}, {}) {
+    _payload = payload;
+}
+
+bool GetChallengePayload::deserialize() {
+    size_t index = 0;
+
+    if (_payload.empty()) return false;
+
+    _opcode = static_cast<OPCODE>(_payload[index++]);
+    if (_opcode != GET_CHALLENGE) return false;
+
+    if (index + sizeof(int) > _payload.size()) return false;
+    memcpy(&_user_len, &_payload[index], sizeof(int));
+    index += sizeof(int);
+    if (_user_len < 0) return false;
+    if (index + _user_len > _payload.size()) return false;
+    _user.resize(_user_len);
+    for (int i = 0; i < _user_len; i++) {
+        _user[i] = _payload[index++];
+    }
+
+    if (index + sizeof(int) > _payload.size()) return false;
+    memcpy(&_challenge_len, &_payload[index], sizeof(int));
+    index += sizeof(int);
+    if (_challenge_len < 0) return false;
+    if (index + _challenge_len > _payload.size()) return false;
+    _challenge.resize(_challenge_len);
+    for (int i = 0; i < _challenge_len; i++) {
+        _challenge[i] = _payload[index++];
+    }
+
+    return true;
+}
+
+GetAuthResponsePayload::GetAuthResponsePayload(std::vector<char> payload)
+    : Payload({}, {}) {
+    _payload = payload;
+}
+
+bool GetAuthResponsePayload::deserialize() {
+    size_t index = 0;
+
+    if (_payload.empty()) return false;
+
+    _opcode = static_cast<OPCODE>(_payload[index++]);
+    if (_opcode != GET_AUTH_RESPONSE) return false;
+
+    if (index + sizeof(int) > _payload.size()) return false;
+    std::memcpy(&_user_len, &_payload[index], sizeof(int));
+    index += sizeof(int);
+    if (_user_len < 0) return false;
+    if (index + _user_len > _payload.size()) return false;
+    _user.resize(_user_len);
+    for (int i = 0; i < _user_len; i++) {
+        _user[i] = _payload[index++];
+    }
+
+    if (index >= _payload.size()) return false;
+    _response = _payload[index++];
+    return true;
+}
+
+PostPublicAndSaltRequestPayload::PostPublicAndSaltRequestPayload(
+    std::string user)
+    : Payload(POST_PUB_AND_SALT_REQUEST, user) {}
+
+std::vector<char> PostPublicAndSaltRequestPayload::serialize() {
+    std::vector<char> buff;
+    buff.push_back(_opcode);
+    add_int(buff, _user_len);
+    for (char c : _user) {
+        buff.push_back(c);
+    }
+    return buff;
+}
