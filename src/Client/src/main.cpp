11a9ca26f216cd199a9b9542de9690daa1463a2c
Kessel <andreidanielcodreanu@gmail.com>
2026-01-12 18:40:10 +0200

    update main to remove argc check

--- src/Client/src/main.cpp
+++ src/Client/src/main.cpp
@@ -1,13 +1,11 @@
 #include "../include/client.hxx"
 
-int main(int argc, char** argv) {
+int main() {
     Comms& conn = Comms::get();
     Client client(conn);
     try {
-        if (argc == 1) client.start_cli(HOSTNAME, PORT);
-        else {
-            // client.start_gui(HOSTNAME, PORT);
-        }
+        client.start_cli(HOSTNAME, PORT);
+
     } catch (const std::exception& e) {
         std::cout << "Unhandled exception: " << e.what() << std::endl;
         return 1;
