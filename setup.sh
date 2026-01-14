bebe604c1b4ff56d9db516b01e52a14e5424fde9
Kessel <andreidanielcodreanu@gmail.com>
2026-01-14 11:37:09 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "[SETUP] Starting setup..."
+
+if ! command -v python3 &> /dev/null; then
+    echo "[SETUP] Python3 could not be found. Please install Python3 first..."
+    exit 1
+fi
+
+if [ ! -d "venv" ]; then
+    echo "[SETUP] Creating virtual environment..."
+    python3 -m venv venv
+else
+    echo "[SETUP] Virtual environment already exists..."
+fi
+
+echo "[SETUP] Activating virtual environment..."
+source ./venv/bin/activate
+
+echo "[SETUP] Upgrading pip..."
+pip install --upgrade pip
+
+if [ ! -f requirements.txt ]; then
+    echo "[SETUP] requirements.txt not found - get it from https://www.github.com/RexGloriae/SilentVault ..."
+    deactivate
+    exit 1
+fi
+
+echo "[SETUP] Installing dependencies from requirements.txt..."
+pip install -r requirements.txt
+
+echo "[SETUP] Creating necessary directories..."
+mkdir ./src/Server/bin
+mkdir ./src/Server/data
+mkdir ./src/Server/crypto
+mkdir ./src/Client/bin
+mkdir ./src/Client/data
+
+echo "[SETUP] Setting up server certificates..."
+./src/Server/setup.sh
+
+echo "[SETUP] Installation completed successfully..."
+echo "[SETUP] Exiting..."
+
+deactivate
+
+exit 0
